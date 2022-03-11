be5bf48b1c98355ed9fdde1207225e4bf8c03c92
August Black <augustblack@gmail.com>
2022-03-11 11:24:32 -0700

    add defaults where needed

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,8 @@
 export * from './alert'
+export { default as Button } from './button'
 export * from './button'
 export * from './tabs'
+export { default as Icons } from './icons'
 export * from './icons'
 export * from './modal'
 export * from './clientOnlyPortal'
