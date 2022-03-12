c7764071206af7941d91203f682c35558f946ee9
August Black <augustblack@gmail.com>
2022-03-12 14:06:58 -0700

    make <Tabs /> example look better

--- pages/tabs.tsx
+++ pages/tabs.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { FC, ReactNode } from 'react'
 import { Tabs, Tab } from '../components/tabs'
+
+const TabDiv: FC<{children:ReactNode}> = ({
+  children
+}) => <div className="p-2">{children}</div>
 
 const TabPage = () => {
   return (
@@ -12,16 +16,45 @@ const TabPage = () => {
           { name: 'icecast', label: 'Icecast' }
         ]}>
           <Tab name='info'>
-            Some info.
+            <TabDiv>
+            Some info. Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+            centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
+            more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </TabDiv>
           </Tab>
           <Tab name='stats'>
-            Some stats
+            <TabDiv>
+            <b>Some Stats.</b> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+            centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
+            more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </TabDiv>
           </Tab>
           <Tab name='record'>
-            Some records
+            <TabDiv>
+            <b>Some Record.</b> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+            centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
+            more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </TabDiv>
+
           </Tab>
           <Tab name='icecast'>
-            Some icecast.
+            <TabDiv>
+            <b>Some Icecast.</b> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+            centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
+            more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </TabDiv>
           </Tab>
         </Tabs>
       </div>
