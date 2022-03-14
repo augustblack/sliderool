cba21a4e54e7c07a05cd5d46fbcfb528ec6fd6d3
August Black <augustblack@gmail.com>
2022-03-13 21:29:40 -0600

    wrap in <div /> so that we can scale internals

--- components/button.tsx
+++ components/button.tsx
@@ -15,22 +15,29 @@ export type ButtonProps = {
 }
 
 const getColorClass = (kind: ButtonKind) => kind === 'plain'
-  ? 'bg-primary-1 hover:bg-primary-2 text-write-1 border border-primary-1 text-primary-4 '
+  ? 'bg-primary-1 hover:bg-primary-2 text-write-1'
   : kind === 'outline' || kind === 'round'
-    ? 'hover:bg-primary-1 bg-base-2 text-primary-3 border border-primary-3 hover:border-primary-4'
-    : '' // for icons, use none and carry color in from container
+    ? 'hover:bg-primary-1 bg-base-2 text-primary-3'
+    : kind === 'group'
+      ? 'hover:bg-primary-1'
+      : '' // for icons, use none and carry color in from container
 
 const getBorderClass = (kind: ButtonKind) => kind === 'plain'
   ? 'border border-primary-1 text-primary-4'
-  : kind === 'outline'
+  : kind === 'outline' || kind === 'round'
     ? 'border border-primary-3 hover:border-primary-4'
-    : ''
+    : kind === 'group'
+      ? 'first:rounded-l last:rounded-r border-1 border-write-1 border-r last:border-none '
+      : ''
 
 const getRoundedClass = (kind: ButtonKind) => kind === 'round'
   ? 'rounded-full'
   : kind !== 'group'
     ? 'rounded-md'
     : ''
+const getExtraClass = (kind: ButtonKind) => kind === 'group'
+  ? 'flex-1'
+  : ''
 
 const Button = React.forwardRef(({
   show = true,
@@ -43,19 +50,28 @@ const Button = React.forwardRef(({
   children
 }: ButtonProps, ref: MutableRefObject<HTMLButtonElement>) => show
   ? (
-    <button
+    <div
       ref={ref}
       disabled={disabled}
       aria-label={label}
       type={type}
       className={[
-        'active:scale-90 p-2 outline-none focus:ring-primary-4 focus:ring-def focus:drop-shadow-def flex items-center justify-center',
+        'flex items-center justify-center ',
         getColorClass(kind),
         getBorderClass(kind),
         getRoundedClass(kind),
+        getExtraClass(kind),
         className
       ].join(' ')}
-      onClick={onClick || null}>{children || label}</button>
+      onClick={onClick || null}
+    >
+    <button
+      ref={ref}
+      disabled={disabled}
+      aria-label={label}
+      type={type}
+      className='active:scale-75 p-2 outline-none focus:ring-primary-4 focus:ring-def focus:drop-shadow-def flex items-center justify-center w-full'
+      >{children || label}</button></div>
     )
   : null
 )
