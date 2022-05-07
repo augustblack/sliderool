7e6bae38dea6e4eabed2be51ab36b13dc2a2a9f9
August Black <augustblack@gmail.com>
2022-05-07 16:08:24 -0600

    separate button type from plain modal, contain state in button types

--- components/modal.tsx
+++ components/modal.tsx
@@ -17,7 +17,6 @@ const getLevel = (lev:number, num:number) => lev === 1
     : ({})
 
 export type ModalProps = {
-  buttonProps: ButtonProps
   level ?:number
   open: boolean
   setOpen: React.Dispatch<React.SetStateAction<boolean>>
@@ -25,14 +24,12 @@ export type ModalProps = {
 }
 
 export const Modal:FC<ModalProps> = ({
-  buttonProps,
   level = 1,
   open,
   setOpen,
   children
 }) => {
   const modalRef = useRef<HTMLDivElement>(null)
-  const buttonRef = useRef<HTMLButtonElement>(null)
   const [state, setState] = useState({
     open: false,
     style: {
@@ -63,9 +60,6 @@ export const Modal:FC<ModalProps> = ({
           : null
       }, 1)
     } else if (mounted) {
-      if (buttonRef.current) {
-        buttonRef.current.focus()
-      }
       setState(s => ({
         open: true,
         style: Object.assign({}, s.style, { opacity: 0.0 })
@@ -79,43 +73,34 @@ export const Modal:FC<ModalProps> = ({
       mounted = false
     }
   }, [open])
-  return (
-    <>
-      <Button
-        ref={buttonRef}
-        onClick={() => setOpen(true)}
-        {...buttonProps}
-      />
-      { state.open
-        ? (
-          <ClientOnlyPortal>
-            <div
-              className={'fixed w-full h-full top-0 left-0 flex justify-center select-none'}
-              style={Object.assign({}, state.style, getLevel(level, 1))}
-            >
-              <div
-                className={'absolute w-full h-full bg-gray-900 opacity-50'}
-                style={getLevel(level, 2)}
-                onMouseDown={() => setOpen(false)}
-              />
-              <div
-                ref={modalRef}
-                role='dialog'
-                aria-modal={true}
-                tabIndex={0}
-                className={'bg-base-1 text-write-1 shadow-lg w-full h-full m-0 md:w-auto md:h-auto md:rounded md:m-auto'}
-                style={getLevel(level, 3)}
-                onKeyDown={esc}
-              >
-                {children}
-              </div>
-            </div>
-          </ClientOnlyPortal>
-          )
-        : null
-      }
-    </>
-  )
+
+  return state.open
+    ? (
+      <ClientOnlyPortal>
+        <div
+          className={'fixed w-full h-full top-0 left-0 flex justify-center select-none'}
+          style={Object.assign({}, state.style, getLevel(level, 1))}
+        >
+          <div
+            className={'absolute w-full h-full bg-gray-900 opacity-50'}
+            style={getLevel(level, 2)}
+            onMouseDown={() => setOpen(false)}
+          />
+          <div
+            ref={modalRef}
+            role='dialog'
+            aria-modal={true}
+            tabIndex={0}
+            className={'bg-base-1 text-write-1 shadow-lg w-full h-full m-0 md:w-auto md:h-auto md:rounded md:m-auto'}
+            style={getLevel(level, 3)}
+            onKeyDown={esc}
+          >
+            {children}
+          </div>
+        </div>
+      </ClientOnlyPortal>
+      )
+    : null
 }
 
 export type ModalWithCloseProps = ModalProps & {
@@ -123,14 +108,13 @@ export type ModalWithCloseProps = ModalProps & {
 }
 
 export const ModalWithClose: FC<ModalWithCloseProps> = ({
-  buttonProps,
   level = 1,
   menu,
   open,
   setOpen,
   children
 }) => (
-  <Modal open={open} setOpen={setOpen} level={level} buttonProps={buttonProps}>
+  <Modal open={open} setOpen={setOpen} level={level}>
     <div className="flex flex-col w-full h-full" style={getLevel(level, 4)} >
       <div className="flex flex-row flex-none items-start">
         { menu
@@ -146,3 +130,96 @@ export const ModalWithClose: FC<ModalWithCloseProps> = ({
     </div>
   </Modal>
 )
+
+export type ModalButtonProps = {
+  buttonProps: ButtonProps
+  level ?:number
+  children?: ReactNode
+}
+
+export const ModalButton:FC<ModalButtonProps> = ({
+  buttonProps,
+  level = 1,
+  children
+}) => {
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const mounted = useRef(false)
+  const [open, setOpen] = useState(false)
+  useEffect(() => {
+    mounted.current = true
+    return () => {
+      mounted.current = false
+    }
+  }, [])
+  useEffect(() => {
+    if (open === false && mounted.current && buttonRef.current) {
+      buttonRef.current.focus()
+    }
+  }, [open])
+
+  return (
+    <>
+      <Button
+        ref={buttonRef}
+        onClick={() => setOpen(true)}
+        {...buttonProps}
+      />
+      <Modal open={open} setOpen={setOpen} level={level}>
+        {children}
+      </Modal>
+    </>
+  )
+}
+
+export type ModalButtonWithCloseProps = ModalButtonProps & {
+  menu?: ReactNode
+}
+
+export const ModalButtonWithClose: FC<ModalButtonWithCloseProps> = ({
+  buttonProps,
+  level = 1,
+  menu,
+  children
+}) => {
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const mounted = useRef(false)
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    mounted.current = true
+    return () => {
+      mounted.current = false
+    }
+  }, [])
+
+  useEffect(() => {
+    if (open === false && mounted.current && buttonRef.current) {
+      buttonRef.current.focus()
+    }
+  }, [open])
+
+  return (
+    <>
+      <Button
+        ref={buttonRef}
+        onClick={() => setOpen(true)}
+        {...buttonProps}
+      />
+      <Modal open={open} setOpen={setOpen} level={level}>
+        <div className="flex flex-col w-full h-full" style={getLevel(level, 4)} >
+          <div className="flex flex-row flex-none items-start">
+            { menu
+              ? (<div className='flex-grow'>{menu}</div>)
+              : null
+            }
+            <div className="flex-grow" />
+            <Button kind='none' className="flex-none " onClick={() => setOpen(false)}><Clear /></Button>
+          </div>
+          <div className="flex-grow m-1 md:m-2 h-full overflow-auto" style={getLevel(level, 4)} >
+            {children}
+          </div>
+        </div>
+      </Modal>
+    </>
+  )
+}
