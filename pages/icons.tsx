a2a04483149df33ab44f9e49a06d98ff824d9d62
August Black <augustblack@gmail.com>
2022-05-17 05:40:32 -0600

    add icons page

---
+++ pages/icons.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Icons from '../components/icons'
+
+const IconsPage = () => {
+  return (
+    <div>
+      <div className='bg-base-2 rounded p-4 max-w-96 grid grid-rows-6 gap-4'>
+        <Icons.Headset />
+        <Icons.Mic />
+        <Icons.Edit />
+        <Icons.Broadcast />
+        <Icons.Clear />
+        <Icons.Pause />
+        <Icons.Play />
+        <Icons.Loop />
+        <Icons.Stop />
+        <Icons.Next />
+        <Icons.Prev />
+        <Icons.DragHandle />
+        <Icons.Fullscreen />
+        <Icons.Download />
+        <Icons.Levels />
+        <Icons.List />
+        <Icons.KeyArrowDown />
+        <Icons.Delete />
+        <Icons.Pending />
+        <Icons.DoubleArrow />
+        <Icons.DoNotDisturb />
+      </div>
+    </div>
+  )
+}
+
+export default IconsPage
