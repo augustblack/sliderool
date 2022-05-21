9dfcde6e49d1e9f760c9d70233a8f34f7fa84c16
August Black <augustblack@gmail.com>
2022-05-21 15:31:27 -0600

    test new slider in overflow

--- pages/slider.tsx
+++ pages/slider.tsx
@@ -7,14 +7,20 @@ const SliderGroup = () => {
   const [vol, setVol] = useState(0)
   const formatFunc = (v: number) => v.toFixed(2).toString()
   return (
-   <div className='bg-gradient-to-b from-red-300 to-red-400 p-4 w-64 rounded flex flex-row'>
-          <Slider value={vol} onChange={setVol} formatFunc={formatFunc} min={0} max={1.5} scale={ScaleType.Log}>
-            <div className='rounded w-12 flex justify-center'>vol</div>
-          </Slider>
-          <Slider value={vol} onChange={setVol} orientation='horizontal' trackSize={150} >
-            <div className='rounded w-12 flex justify-center'>vol</div>
-          </Slider>
-        </div>
+   <div className='bg-gradient-to-b from-red-300 to-red-400 p-4 rounded flex flex-row space-x-6'>
+     <Slider value={vol} onChange={setVol} formatFunc={formatFunc} min={0} max={1.5} scale={ScaleType.Log}>
+       <div className='rounded w-12 flex justify-center'>vol</div>
+     </Slider>
+     <div className='flex flex-col space-y-6'>
+       <Slider value={vol} onChange={setVol} orientation='horizontal' trackSize={150} >
+         <div className='rounded w-12 flex justify-center'>vol</div>
+       </Slider>
+       <Slider value={vol} onChange={setVol} orientation='horizontal' trackSize={150} >
+         <div className='rounded w-12 flex justify-center'>vol</div>
+       </Slider>
+     </div>
+
+   </div>
   )
 }
 
@@ -59,77 +65,25 @@ const Sliders = () => {
   ])
   const orderPlaylist = (p: Playlist) => setPlaylist(p)
   return (
-    <div>
-      <div className='flex flex-row bg-base-2 rounded p-4 space-x-2'>
+    <div className='flex flex-col space-y-6'>
+      <div className='flex flex-row bg-base-2 rounded p-4 space-x-2 overflow-x-auto w-96 '>
+        <SliderGroup />
+        <SliderGroup />
+        <SliderGroup />
+        <SliderGroup />
+        <SliderGroup />
         <SliderGroup />
       </div>
+      { /* include this to check for layout bug in framer-motion
+          that seems to happen when we use 'layout' on  motion.div
+         */ }
       <PlaylistDnD
         playlist={playlist}
         orderPlaylist={orderPlaylist}
-    />
+      />
 
     </div>
   )
 }
-/*
-const Sliders = () => {
-  const [vol, setVol] = useState(0)
-  const mounted = useRef(false)
-  useEffect(() => {
-    mounted.current = true
-    return () => { mounted.current = false }
-  }, [])
-  console.log('sliders')
 
-  const volSlider = useSlider({
-    mounted: mounted.current,
-    value: vol,
-    onChange: setVol,
-    min: 0,
-    max: 1.3,
-    scale: ScaleType.Log
-  })
-
-  return (
-    <div>
-      <div className='flex flex-row bg-base-2 rounded p-4 space-x-2'>
-        <div className='bg-gradient-to-b from-red-300 to-red-400 p-4 w-64 rounded'>
-          <Slider slider={volSlider} trackSize={220} >
-            <div className='rounded w-12 flex justify-center'>vol</div>
-          </Slider>
-        </div>
-        <div className='flex flex-col space-y-2'>
-          <Slider slider={volSlider} trackSize={220} orientation='horizontal' >
-            <div className='rounded w-12 flex justify-center'>volH</div>
-          </Slider>
-          <Slider slider={volSlider} trackSize={120} orientation='horizontal' >
-            <div className='rounded w-12 flex justify-center'>volH2</div>
-          </Slider>
-        </div>
-      </div>
-      <div className='bg-base-2 rounded p-4 space-y-2'>
-        <ModalButtonWithClose buttonProps={{ label: 'plain modal', kind: 'plain' }}>
-          <div className='flex flex-row bg-base-2 rounded p-4 space-x-2' style={{ zIndex: 99 }}>
-            <div className='bg-gradient-to-b from-gray-300 to-gray-400 p-4 w-64 rounded'>
-              <Slider slider={volSlider} trackSize={220} >
-                <div className='rounded w-12 flex justify-center'>vol</div>
-              </Slider>
-            </div>
-            <div className='flex flex-col space-y-2'>
-              <Slider slider={volSlider} trackSize={220} orientation='horizontal' >
-                <div className='rounded w-12 flex justify-center'>volH</div>
-              </Slider>
-              <Slider slider={volSlider} trackSize={120} orientation='horizontal' >
-                <div className='rounded w-12 flex justify-center'>volH2</div>
-              </Slider>
-            </div>
-          </div>
-        </ModalButtonWithClose>
-
-      </div>
-    </div>
-  )
-}
- */
-
-export default React.memo(Sliders)
+export default Sliders
