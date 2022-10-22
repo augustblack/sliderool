449098985a18106ecdd87a6e7f4755a6ed14730e
August Black <augustblack@gmail.com>
2022-10-22 10:36:30 -0600

    ts updates

--- components/alert.tsx
+++ components/alert.tsx
@@ -2,12 +2,11 @@ import React, { FC, ReactNode } from 'react'
 import { Clear } from './icons'
 import Button from './button'
 export type AlertKind = 'info' | 'success' | 'warning' | 'error'
-
 export type AlertProps = {
   children: ReactNode
-  kind ?: AlertKind
-  className ?: string
-  onClose ?: () => void
+  kind?: AlertKind
+  className?: string
+  onClose?: () => void
 }
 const classes: Record<AlertKind, string> = {
   info: 'bg-info-1 border-info-2 text-info-2',
@@ -37,48 +36,52 @@ export const Alert: FC<AlertProps> = ({
       className
     ].join(' ')}
   >
-    <div className='flex-grow p-2'>
-    {children}
-    </div>
-    {
-      onClose
-        ? (
-          <Button
-            kind='none'
-            className={[
-              'flex-shrink',
-              hoverClasses[kind]
-            ].join(' ')}
-            onClick={onClose}
-            ><Clear /></Button>
-          )
-        : null
-    }
+    <div className="flex-grow p-2">{children}</div>
+    {onClose
+      ? (
+        <Button
+          kind="none"
+          className={['flex-shrink', hoverClasses[kind]].join(' ')}
+          onClick={onClose}
+        >
+          <Clear />
+        </Button>
+        )
+      : null}
   </div>
 )
 type ErrorDisplayProps = {
   pre: string
-  errors: Array<string|Error|ErrorEvent>
-  clearErrors ?: () => void
+  errors: Array<string | Error | ErrorEvent>
+  clearErrors?: () => void
 }
-export const ErrorDisplay :FC<ErrorDisplayProps> = ({
+export const ErrorDisplay: FC<ErrorDisplayProps> = ({
   pre,
   errors,
   clearErrors
-}: ErrorDisplayProps) => errors && errors.length >= 1
-  ? (
-    <div className='relative bg-red-100 border border-red-400 text-red-700 p-2 rounded text-base' role='alert'>
-      { clearErrors
-        ? (
-          <button onClick={clearErrors} className='absolute top-0 right-0 p-1 m-1 bg-red-300 rounded' style={{ fill: 'red' }} >
-            <Clear fill='#7F1D1D' />
-          </button>
-          )
-        : null
-      }
-      <div className=''>
-        { errors.map((err, idx) => (<Alert key={pre + idx} >{err}</Alert>)) }
+}: ErrorDisplayProps) =>
+  errors && errors.length >= 1
+    ? (
+      <div
+        className="relative bg-red-100 border border-red-400 text-red-700 p-2 rounded text-base"
+        role="alert"
+      >
+        {clearErrors
+          ? (
+            <button
+              onClick={clearErrors}
+              className="absolute top-0 right-0 p-1 m-1 bg-red-300 rounded"
+              style={{ fill: 'red' }}
+            >
+              <Clear fill="#7F1D1D" />
+            </button>
+            )
+          : null}
+        <div className="">
+          {errors.map((err, idx) => (
+            <Alert key={pre + idx}>{String(err)}</Alert>
+          ))}
+        </div>
       </div>
-    </div>
-    )
-  : null
+      )
+    : null
