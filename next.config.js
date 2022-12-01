3e903753405df00019f93b246fcf400bc3f0b935
August Black <augustblack@gmail.com>
2022-12-01 14:01:42 -0700

    add unsplash to images config so we can demo with them

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@ module.exports = {
   eslint: {
     dirs: ['utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+    ],
+  }
   // productionBrowserSourceMaps: true, // turn this on for production debugging
 }
