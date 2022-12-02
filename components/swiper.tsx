b51a9db680bdf043bdb88c6e14af54ff8954d392
August Black <augustblack@gmail.com>
2022-12-02 04:59:31 -0700

    update <Swiper /> with <Pagingation />

--- components/swiper.tsx
+++ components/swiper.tsx
@@ -1,6 +1,7 @@
 // https://koenvg.medium.com/infinite-carousel-with-framer-motion-b5f93b06ae9a
 import React, { FC, ReactNode, useState, useRef, useEffect } from 'react'
 import { Icons } from '../components'
+import { Pagination } from '../components/pagination'
 import {
   motion,
   MotionStyle,
@@ -11,10 +12,18 @@ import {
   useMotionValue
 } from 'framer-motion'
 
+type SwiperState = {
+  idx: number
+  wrappedIdx: number
+  length: number
+}
+
 
 interface PageProps {
-  index: number
-  renderPage: (props: { index: number }) => ReactNode
+  idx: number
+  wrappedIdx: number
+  length: number
+  renderPage: (props: { idx: number, wrappedIdx: number, length: number }) => ReactNode
   x: MotionValue
   onDragEnd(event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo): void
 }
@@ -25,16 +34,23 @@ const pageStyle: MotionStyle = {
   height: '100%',
 }
 
-export const Page: FC<PageProps> = ({ index, renderPage, x, onDragEnd }) => {
-  const child = React.useMemo(() => renderPage({ index }), [index, renderPage])
+export const Page: FC<PageProps> = ({
+  idx, 
+  length,
+  wrappedIdx,
+  renderPage,
+  x,
+  onDragEnd 
+}) => {
+  const child = React.useMemo(() => renderPage({ idx, wrappedIdx, length }), [idx, wrappedIdx, renderPage])
 
   return (
     <motion.div
       style={{
         ...pageStyle,
         x,
-        left: `${index * 100}%`,
-        right: `${index * 100}%`,
+        left: `${idx * 100}%`,
+        right: `${idx * 100}%`,
       }}
       draggable
       drag="x"
@@ -51,7 +67,8 @@ const range = [-1, 0, 1]
 
 type SwiperProps = {
   index?: number
-  children: (props: {index: number}) => ReactNode
+  length: number
+  children: (props: SwiperState) => ReactNode
 }
 
 const containerStyle: MotionStyle = {
@@ -66,6 +83,16 @@ const transition: AnimationOptions<number> = {
   bounce: 0
 }
 
+const getSwipeState = (index:number, length:number): SwiperState => {
+  const modulo = index % length
+  const wrappedIdx = modulo < 0 ? length + modulo : modulo
+  return {
+    idx: index,
+    wrappedIdx,
+    length
+  }
+}
+
 // const calculateNewX = (i: number, div: HTMLElement ) => -i * (div?.clientWidth || 0)
 const calculateNewX = (i: number, div: HTMLDivElement | null ) => -i * (div?.getBoundingClientRect().width || 0)
 
@@ -76,11 +103,12 @@ const arrowClass = 'text-gray-100 bg-opacity-10 bg-gray-900 cursor-pointer'
 
 export const Swiper: FC<SwiperProps> = ({
   index = 0,
+  length,
   children
 }) => {
   const x = useMotionValue(0)
   const containerRef = useRef<HTMLDivElement>(null)
-  const [idx, setIndex] = useState(index)
+  const [state, setState] = useState<SwiperState>(getSwipeState(index, length))
 
   const handleEndDrag = (_e: Event, dragProps: PanInfo) => {
     const clientWidth = containerRef.current?.clientWidth || 0
@@ -88,55 +116,62 @@ export const Swiper: FC<SwiperProps> = ({
     const { offset, velocity } = dragProps
 
     if (Math.abs(velocity.y) > Math.abs(velocity.x)) {
-      animate(x, calculateNewX(idx, containerRef.current), transition)
+      animate(x, calculateNewX(state.idx, containerRef.current), transition)
       return
     }
 
     if (offset.x > clientWidth / 4) {
-      setIndex(idx - 1)
+      setState(s => getSwipeState(s.idx - 1, s.length))
     } else if (offset.x < -clientWidth / 4) {
-      setIndex(idx + 1)
+      setState(s => getSwipeState(s.idx + 1, s.length))
     } else {
-      animate(x, calculateNewX(idx, containerRef.current), transition)
+      animate(x, calculateNewX(state.idx, containerRef.current), transition)
     }
   }
 
   useEffect(() => {
     // need to set resize here
     const onResize = () => {
-      x.set(calculateNewX(idx, containerRef.current))
+      x.set(calculateNewX(state.idx, containerRef.current))
     }
     window.addEventListener('resize', onResize)
     onResize()
     return () => {
       window.removeEventListener('resize', onResize)
     }
-  }, [idx, x])
+  }, [state.idx, x])
 
   useEffect(() => {
-    const controls = animate(x, calculateNewX(idx, containerRef.current), transition)
+    const controls = animate(x, calculateNewX(state.idx, containerRef.current), transition)
     return controls.stop
-  }, [idx, x])
+  }, [state.idx, x])
 
-  const goLeft = () => setIndex(idx => idx - 1)
-  const goRight = () => setIndex(idx => idx + 1)
-
+  const goLeft = () => setState(s => getSwipeState(s.idx - 1, s.length))
+  const goRight = () => setState(s => getSwipeState(s.idx + 1, s.length))
 
   return (
-    <motion.div ref={containerRef} style={containerStyle} className="flex items-center">
+    <motion.div ref={containerRef} style={containerStyle} className="flex items-center pb-16">
       <button className={iconClassL} onClick={goLeft}><Icons.ChevronLeft size='4rem' className={arrowClass} /></button>
       {range.map((rangeValue) => {
+        const rngState = rangeValue === 0 
+          ? state
+          : getSwipeState(state.idx + rangeValue, state.length)
         return (
           <Page
-            key={rangeValue + idx}
+            key={rngState.idx}
             x={x}
             onDragEnd={handleEndDrag}
-            index={rangeValue + idx}
+            idx={rngState.idx}
+            length={rngState.length}
+            wrappedIdx={rngState.wrappedIdx}
             renderPage={children}
           />
         )
       })}
       <button className={iconClassR} onClick={goRight}><Icons.ChevronRight size='4rem' className={arrowClass} /></button>
+      <div className='absolute z-10 left-0 bottom-0 h-8 text-xs w-full flex place-items-center place-content-center '>
+        <Pagination currentIdx={state.wrappedIdx} length={state.length} keyPre={'ssss'} onChange={(n) => setState(s => getSwipeState(n, s.length))} />
+      </div>
     </motion.div>
   )
 }
--- pages/swipers.tsx
+++ pages/swipers.tsx
@@ -44,6 +44,10 @@ const tracks: Array<Track> = [
     id: 'skkkso',
     kind: 'player',
     vol: 0.3,
+    img: {
+      src: 'https://unsplash.com/photos/XIVDN9cxOVc/download?force=true&w=640',
+      description: 'hacker'
+    },
     bg: 'bg-green-200'
   },
   {
@@ -58,25 +62,23 @@ const Home: NextPage = () => {
     <div className='w-full h-full bg-red-100 flex flex-row'>
       <div className='flex-grow' />
       <div className='w-full md:w-5/6 lg:w-3/4 h-full bg-blue-100'>
-        <Swiper>
-          {({ index }) => {
-            const modulo = index % tracks.length
-            const tidx = modulo < 0 ? tracks.length + modulo : modulo
-            const t = tracks[tidx]
+        <Swiper length={tracks.length}>
+          {({ wrappedIdx }) => {
+            const t = tracks[wrappedIdx]
             return (
               <div key={t.id} className={'w-full h-full ' + t.bg + ' relative items-center justify-center'}>
               {t.img && <Image
-                className='rounded'
+                className='object-cover'
                 draggable={false}
                 src={t.img.src}
                 alt={t.img.description}
                 // placeholder='blur'
-                layout='fill'
-                objectFit='cover'
-                objectPosition={t.img.position || 'center'}
+                fill={true}
+                style={{
+                  objectPosition: t.img.position || 'center'
+                }}
               />
               }
-
               </div>
             )
           }}
