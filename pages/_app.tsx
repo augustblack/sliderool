2ea407a2a9fdceda0431431636288948d5278ba9
August Black <augustblack@gmail.com>
2023-02-23 15:21:13 -0700

    move body dark mode setting to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import React, { FC, ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
+import { Button } from '../components'
 
 type NavProps = {
   children?: ReactNode
 }
 
 const Menu = () => (
-  <div className='flex flex-col space-y-4 p-4' >
+  <div className='flex flex-col gap-2 lg:gap-4 px-1 lg:px-4' >
     <Link href='/alerts'>Alerts</Link>
     <Link href='/accordians'>Accordians</Link>
     <Link href='/swipers'>Swipers</Link>
@@ -21,6 +22,7 @@ const Menu = () => (
     <Link href='/forms'>Forms</Link>
     <Link href='/icons'>Icons</Link>
     <Link href='/sliders'>Sliders</Link>
+    <Link href='/playlists'>Playlists</Link>
     <div>----</div>
     <Link href='/tracks'>Tracks</Link>
     <Link href='/tracksUp'>TracksUp</Link>
@@ -35,11 +37,23 @@ const Template: FC<NavProps> = ({
     <>
       <Head>
         <title>s l i d e r o o l</title>
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1"
+          />
       </Head>
-      <div className='flex flex-row space-x-8 p-4 h-full'>
-        <Menu />
-        <div className='space-y-2 items-center w-full h-full' >
-          {children}
+      <div className='flex flex-col space-x-2 h-full w-full '>
+        <div className='flex flex-row space-x-2 p-2 w-full'>
+          <div className='flex-grow' />
+          <Button kind='plain' label='dark' onClick={ () => { document.body.classList.remove('light'); document.body.classList.add('dark') }} />
+          <Button kind='plain' label='light' onClick={ () => { document.body.classList.remove('dark'); document.body.classList.add('light')}} />
+        </div>
+
+        <div className='flex flex-row gap-2 md:gap-8 p-2 md:p-4 h-full w-full'>
+          <Menu />
+          <div className='gap-0 lg:gap-2 items-center w-full' >
+            {children}
+          </div>
         </div>
       </div>
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
-import Button from '../components/button'
 
 export default function Document() {
-  const [theme, setTheme] = useState('light')
-  useEffect(() => {
-    document.body.className = theme
-  }, [theme])
-
-
   return (
     <Html>
       <Head>
@@ -20,16 +13,8 @@ export default function Document() {
         />
         <link rel="icon" type="image/x-icon" href={'/favicon.ico'} />
       </Head>
-      <body className={' p-4 bg-base-3 text-write-1 space-y-4 w-screen h-screen'} style={{ transition: 'background-color .25s' }}>
-        <div className='flex flex-col space-x-2 h-full '>
-          <div className='flex flex-row space-x-2 p-2'>
-            <div className='flex-grow' />
-            <Button kind='plain' label='dark' onClick={() => setTheme('dark')} />
-            <Button kind='plain' label='light' onClick={() => setTheme('light')} />
-          </div>
-          <Main />
-        </div>
-
+      <body className='bg-base-3 text-write-1 w-screen h-screen ' style={{ transition: 'background-color .25s' }}>
+        <Main />
         <NextScript />
       </body>
     </Html>
