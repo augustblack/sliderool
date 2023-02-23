f5833115033be86b484c1349ae7ef8a6ff14d68c
August Black <augustblack@gmail.com>
2023-02-23 15:20:19 -0700

    add playlists

---
+++ pages/playlists.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { PlaylistDnD, Playlist } from '../components/playlist'
+
+const Playlists = () => {
+  const [playlist, setPlaylist] = useState<Playlist>([
+    {
+      isQueued: true,
+      hasBuffer: false,
+      key: 'aldksjfa',
+      uri: 'wtf',
+      title: 'you there'
+    },
+    {
+      isQueued: false,
+      hasBuffer: false,
+      key: 'aldkalfdjas',
+      uri: 'wtf2',
+      title: 'you hellow there'
+    },
+    {
+      isQueued: false,
+      hasBuffer: false,
+      key: 'asldfjkallls',
+      uri: 'wtsss',
+      title: 'you brown there'
+    },
+    {
+      isQueued: false,
+      hasBuffer: false,
+      key: 'aldkxxlfdjas',
+      uri: 'wtf2xx',
+      title: 'you xxx there'
+    },
+    {
+      isQueued: false,
+      hasBuffer: false,
+      key: 'aldal',
+      uri: 'wtf2',
+      title: 'you hellow there'
+    }
+
+  ])
+  const orderPlaylist = (p: Playlist) => setPlaylist(p)
+  return (
+    <div className='flex flex-row bg-base-2 rounded p-2 lg:p-4 gap-2 lg:gap-4 overflow-x-auto w-full '>
+      <PlaylistDnD playlist={playlist} orderPlaylist={orderPlaylist} />
+    </div>
+  )
+}
+
+export default Playlists
