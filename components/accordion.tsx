edf5557ac4b8ec878130a51e49131c281396b68a
August Black <augustblack@gmail.com>
2023-12-25 14:48:02 -0700

    adjust and remove framer code

--- components/accordion.tsx
+++ components/accordion.tsx
@@ -1,6 +1,4 @@
 import React, { FC, ReactNode } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
-// import Icons, { IconOnOffProps, IconProps } from './icons'
 
 export type AccordionProps = {
   id: string
@@ -13,13 +11,13 @@ export type AccordionProps = {
 }
 
 export const Accordion: FC<AccordionProps> = ({
-    id,
-    expanded,
-    setExpanded,
-    header,
-    headerClass='',
-    childClass='',
-    children
+  id,
+  expanded,
+  setExpanded,
+  header,
+  headerClass = '',
+  childClass = '',
+  children
 }) => {
   const isOpen = id === expanded
 
@@ -32,52 +30,10 @@ export const Accordion: FC<AccordionProps> = ({
         onClick={() => setExpanded(isOpen ? '' : id)}
       >{header}</div>
       <div
-          className={'overflow-hidden transition-all duration-700 ' + childClass + (isOpen ? 'max-h-60' : 'max-h-0' )}
+        className={'overflow-hidden transition-all duration-700 ' + childClass + (isOpen ? 'max-h-60' : 'max-h-0')}
       >
         {children}
       </div>
     </>
   )
 }
-
-export const AccordionOld: FC<AccordionProps> = ({
-    id,
-    expanded,
-    setExpanded,
-    header,
-    headerClass='',
-    childClass='',
-    children
-}) => {
-  const isOpen = id === expanded;
-
-  // By using `AnimatePresence` to mount and unmount the contents, we can animate
-  // them in and out while also only rendering the contents of open accordions
-  return (
-    <>
-      <motion.div
-        layout
-        className={'cursor-pointer ' + headerClass}
-        initial={false}
-        onClick={() => setExpanded(isOpen ? '' : id)}
-      >{header}</motion.div>
-      <AnimatePresence initial={false}>
-        {isOpen && (
-          <motion.div
-            className={'overflow-hidden ' + childClass}
-            initial="collapsed"
-            animate="open"
-            exit="collapsed"
-            variants={{
-              open: { opacity: 1, height: "auto" },
-              collapsed: { opacity: 0, height: 0 }
-            }}
-            transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
-          >
-            {children}
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </>
-  );
-};
