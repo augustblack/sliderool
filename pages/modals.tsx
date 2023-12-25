fbdb7c41e43befd6fbc0852dbbc06ac3effbd0a8
August Black <augustblack@gmail.com>
2023-12-25 14:56:45 -0700

    remove framer

--- pages/modals.tsx
+++ pages/modals.tsx
@@ -3,7 +3,6 @@ import { Template } from '../shared'
 // import { IconToggle, Toggle } from '../components/toggle'
 import Icons from '../components/icons'
 import { Modal, ModalWithClose, ModalButton, ModalButtonWithClose, ButtonWithFocus } from '../components/modal'
-import { FModalButton, FModalButtonWithClose } from '../components/fmodal'
 import Button from '../components/button'
 
 const Modals = () => {
@@ -34,8 +33,8 @@ const Modals = () => {
       </div>
       <div className='bg-base-2 rounded p-4 space-y-2'>
         <div className='flex flex-row space-x-2'>
-          <ButtonWithFocus open={showTwo} setOpen={setShowTwo} buttonProps={{ label: 'modal w/close', kind: 'plain' }} />
-          <ModalWithClose open={showTwo} setOpen={setShowTwo}>
+          <ButtonWithFocus open={showTwo} setOpen={setShowTwo} buttonProps={{ label: 'modal w/close full', kind: 'plain' }} />
+          <ModalWithClose open={showTwo} setOpen={setShowTwo} full={true}>
             <div className='w-96 p-4'>
               <div>Hello. This is a longer text to fill out the space.</div>
               <div>It&apos;s possible you would likely have some buttons and</div>
@@ -109,43 +108,6 @@ const Modals = () => {
           </ModalButtonWithClose>
         </div>
       </div>
-      <div className='bg-base-2 rounded p-4 space-y-2'>
-        <div className='flex flex-row space-x-2'>
-          <FModalButton buttonProps={{ label: 'framer button modal', kind: 'plain' }}>
-            <div className='p-4'>
-              Hello. This is a modal or also known as a dialog. This one is plain with nuthin in it. It should still have focus on open.
-              You should be able to hit ESC to close.  The focus should then return to the button.
-            </div>
-          </FModalButton>
-          <FModalButtonWithClose
-            buttonProps={{ label: 'modal w/menu', kind: 'plain' }}
-            menu={(
-              <div className='flex flex-row space-x-2 pl-4 pt-3'>
-                <Button kind='round' className='w-12 h-12 bg-base-1'>
-                  <Icons.Headset size='36px' />
-                </Button>
-                <Button kind='round' className='w-12 h-12 bg-base-3'>
-                  <Icons.Play size='24px' />
-                </Button>
-                <Button kind='round' className='w-12 h-12'>
-                  <Icons.Pending size='24px' />
-                </Button>
-                <Button kind='round' className='w-10 h-10'>
-                  <Icons.List size='24px' />
-                </Button>
-              </div>
-            )}
-
-          >
-            <div className='w-full h-full p-4'>
-              Hello. This is a longer text to fill out the space.  It&apos;s possible you would likely have some buttons and other schnick schnock in here
-              to make things seem fuller and funner.
-            </div>
-          </FModalButtonWithClose>
-
-        </div>
-      </div>
-
     </Template>
   )
 }
