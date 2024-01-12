1a44c493a43e061a35b02341113fb00291d634b9
August Black <augustblack@gmail.com>
2024-01-11 18:02:54 -0700

    utils.js

---
+++ components/utils.ts
@@ -0,0 +1,2 @@
+export type Colors = { track: string, thumb: string }
+export const baseRingClass = 'outline-none ring-0 ring-offset-0 ring-inset focus:ring-def focus:ring-offset-0 focus:ring-inset '
